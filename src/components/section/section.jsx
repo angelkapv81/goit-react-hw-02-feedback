cd7c1a1f360c172e3c0e3277a7b50c6e806f3e99
angelkapv81@gmail.com <angelkapv81>
2023-05-08 12:58:11 +0300

    fix

--- src/components/section/section.jsx
+++ src/components/section/section.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import style from './Section.module.css';
 export const Section = ({ title, children }) => (
   <section>
     <h2 className={style.title}>{title}</h2>
